926b66194eb0ef10015f9f89f8ccf7aed4714b75
SSalerno <skylar.salerno@gmail.com>
2018-09-13 16:05:15 -0400

    Added ability comment

--- App.js
+++ App.js
@@ -5,12 +5,20 @@ const bodyParser=require('body-parser');
 const fs=require('fs');
 let topics=[];
 app.set('view engine','pug');
+
 app.use(bodyParser.urlencoded({extended: false}));
+
 app.get('/', (req, res) => {res.render('index',{topics:topics})});
-app.post('/comment'),(req, res)=>{topics[parseInt(req.body.topicNum)].commentsreq.body.comment}
-app.post('/', (req, res) =>{let topicObj={heading:req.body.topic};topics.push(topicObj);
-console.log(topics);
-res.render('index',{topics:topics})});
+
+
+
+app.post('/', (req, res) =>{let topic={heading:req.body.heading, comments:[]};topics.push(topic);res.redirect('/');});
+
+app.post('/comment',(req, res)=>{console.log(req.body.comment);
+topics[parseInt(req.body.topicNum)].comments.push(req.body.comment);
+res.redirect('/');});
+
+
 function savetoDisk(){ if(topics.length) {fs.writeFile('./content.json', JSON.stringify(topics), 'utf-8', function(err) {
 		if (err) throw err
 		console.log('Done!')
